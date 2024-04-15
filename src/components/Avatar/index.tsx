a2bb9a6574e422b4761ddbba4a50e1205b5f8396
carlosmellocarvalho <carlos.hmc@gmail.com>
2024-04-15 18:42:45 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/caiquemellocarvalho.png" alt="" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/caiquemellocarvalho.png" />
-    <Titulo fontSize={20}>Carlos Mello Carvalho</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Carlos Mello Carvalho</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        caiquemellocarvalho
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
