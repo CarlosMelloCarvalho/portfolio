6abd5727a95be42bf36b5d527ed8df325db12b57
carlosmellocarvalho <carlos.hmc@gmail.com>
2024-04-15 16:03:22 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/caiquemellocarvalho.png" />
+    <Titulo fontSize={20}>Carlos Mello Carvalho</Titulo>
+  </aside>
+)
+
+export default Sidebar
